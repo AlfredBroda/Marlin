c2cc8e6dbf1ba9c0bf3e9136741d24bc81b3176a
Alfred Broda <alfred.broda@gmail.com>
2021-03-10 17:45:32 +0100

    Enable power loss recovery
    Enable power loss recovery

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1042,7 +1042,7 @@
 #define XY_PROBE_SPEED (133*60)
 
 // Feedrate (mm/m) for the first approach when double-probing (MULTIPLE_PROBING == 2)
-#define Z_PROBE_SPEED_FAST 60*60
+#define Z_PROBE_SPEED_FAST HOMING_FEEDRATE_Z
 
 // Feedrate (mm/min) for the "accurate" probe of each point
 #define Z_PROBE_SPEED_SLOW (Z_PROBE_SPEED_FAST / 2)
